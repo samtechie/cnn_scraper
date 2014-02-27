dedd74cbe08e0c5804efce98e2fd3852cdea1bc7
Wasswa Samuel <latimerscope@gmail.com>
2014-02-28 00:33:20 +0300

    Finished up view and scraping

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
+require 'open-uri'
 class StaticPagesController < ApplicationController
   def home
-  end
+  	@links = []
+  	@articles = []
+  	agent = Mechanize.new
+    page = agent.get("http://edition.cnn.com/?refresh=1")
+    page.search("//div[@id='cnn_maintt2bul']/div/div/ul/li[count(*)=3]/a")[0, 5].map{|a| page.uri.merge a[:href]}.each do |uri| 
+    article = agent.get(uri).parser
+    @links << uri
+    @articles << article.css(".adtag15090+ p").text
+    end
+   end
 end
