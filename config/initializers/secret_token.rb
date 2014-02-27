8c45dcf5ae585e4a1e066e73cf5c72907b4c2095
Wasswa Samuel <latimerscope@gmail.com>
2014-02-27 10:51:14 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CnnScraper::Application.config.secret_key_base = '47d491cccd9a12dfe271b94a64f1acde7ddd1f4701517ddcdbb864cc68d8c71855ed0000a607fa9053131080ee4a823530277d8fee0dfd59a98ae8eab3dd7f3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CnnScraper::Application.config.session_store :cookie_store, key: '_cnn_scraper_session'
